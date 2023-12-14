f9c4ccc0f1e767524200645755be4be270e1b3a5
Developer Seetings <developersettings007@gmail.com>
2023-12-14 14:47:19 -0500

    fixed bug that prevent from. build in production

--- app/management/employees/page.tsx
+++ app/management/employees/page.tsx
@@ -1,3 +1,4 @@
+import { EmployeeProfile } from '@/app/types/definitions';
 import { columns } from './components/columns';
 import { DataTable } from './components/data-table';
 import { getEmployees } from './utils';
--- app/utils/query.ts
+++ app/utils/query.ts
@@ -1,2 +1,5 @@
+import { neon } from '@neondatabase/serverless';
+
 import postgres from 'postgres';
 export const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
+// neon(process.env.DATABASE_URL!);
